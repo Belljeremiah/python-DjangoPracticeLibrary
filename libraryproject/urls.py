d01b2d0a0069e7f97810d2f24067e5078e132929
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2020-02-06 15:13:34 -0600

    views

--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.urls import path
 from libraryapp.models import *
 
 """libraryproject URL Configuration
@@ -20,4 +23,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('libraryapp.urls')),
 ]
